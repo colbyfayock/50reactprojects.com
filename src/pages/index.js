5ba47b3762be7539b7766d706d2666810fe7830f
colbyfayock <fayock@gmail.com>
2020-08-08 18:28:43 -0400

    style updates, adding list icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
 import Head from 'next/head'
+import {
+  FaBriefcase,
+  FaAddressBook,
+  FaPuzzlePiece,
+  FaProjectDiagram,
+  FaFlask,
+  FaLeaf,
+  FaTools,
+  FaRegClone
+} from 'react-icons/fa';
+
 import styles from '../styles/Home.module.css'
 
 import LogoReact from '../components/LogoReact';
@@ -31,6 +42,7 @@ export default function Home() {
                 Get the <strong>FREE EBOOK</strong> straight to your inbox!
               </p>
               <input type="hidden" name="tags[]" value="1749930" />
+              <label className={styles.sronly} htmlFor="email">Email Address</label>
               <input type="email" name="email_address" placeholder="Email Address" />
               <button>Get It Free</button>
             </form>
@@ -45,7 +57,8 @@ export default function Home() {
             <h2>Don&apos;t Ask &quot;What to Build&quot;</h2>
 
             <p className={styles.subhead}>
-              Dive in with 50 projects complete with project briefs and wireframes!
+              Dive in with <strong>50 projects</strong> complete
+              with <strong className={styles.blue}>project briefs</strong> and <strong className={styles.purple}>wireframes</strong>!
             </p>
 
             <img src="/images/50-react-projects-business-real-world-ecommerce-store.png" alt="" />
@@ -59,23 +72,49 @@ export default function Home() {
             <h2>Learn by Doing</h2>
 
             <p>
-              One of the best ways to learn is by doing the work. Choose from
-              8 project categories and get started right away.
+              One of the best ways to learn is by <strong>doing the work</strong>. Choose
+              from <strong>8 project categories</strong> and get started right away.
             </p>
 
-            <ul className={styles.topics}>
-              <li>Business &amp; Real-World</li>
-              <li>Personal &amp; Portfolio</li>
-              <li>Games &amp; Puzzles</li>
-              <li>Project Add-Ons</li>
-              <li>Fun &amp; Interesting</li>
-              <li>Productivity</li>
-              <li>Tools &amp; Libraries</li>
-              <li>Clones</li>
-            </ul>
+            <div className={styles.topics}>
+              <ul>
+                <li>
+                  <span><FaBriefcase /></span>
+                  Business &amp; Real-World
+                </li>
+                <li>
+                  <span><FaAddressBook /></span>
+                  Personal &amp; Portfolio
+                </li>
+                <li>
+                  <span><FaPuzzlePiece /></span>
+                  Games &amp; Puzzles
+                </li>
+                <li>
+                  <span><FaProjectDiagram /></span>
+                  Project Add-Ons
+                </li>
+                <li>
+                  <span><FaFlask /></span>
+                  Fun &amp; Interesting
+                </li>
+                <li>
+                  <span><FaLeaf /></span>
+                  Productivity
+                </li>
+                <li>
+                  <span><FaTools /></span>
+                  Tools &amp; Libraries
+                </li>
+                <li>
+                  <span><FaRegClone /></span>
+                  Clones
+                </li>
+              </ul>
+            </div>
 
             <p className={styles.subhead}>
-              Keep building and level up!
+              Keep building and <strong>level up</strong>!
             </p>
 
           </div>
@@ -92,7 +131,8 @@ export default function Home() {
 
             <form className={styles.form} data-theme="light" action="https://app.convertkit.com/forms/1539376/subscriptions" method="post">
               <input type="hidden" name="tags[]" value="1749930" />
-              <input type="email" name="email_address" placeholder="Email Address" />
+              <label className={styles.sronly} htmlFor="email">Email Address</label>
+              <input id="email" type="email" name="email_address" placeholder="Email Address" />
               <button>Get It Free</button>
             </form>
 
