de2bb67a2f11518d2eea8fa2295acb988a320d14
Colby Fayock <fayock@gmail.com>
2020-08-19 12:26:20 -0400

    producthunt badge

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,6 +106,15 @@ export default function Home() {
               </p>
             </form>
 
+            <p className={styles.producthunt}>
+              <a href="https://www.producthunt.com/posts/50-projects-for-react-the-static-web?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-50-projects-for-react-the-static-web" target="_blank">
+                <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=238203&theme=light" alt="50 Projects For React & The Static Web - Learn by doing with this FREE ebook! | Product Hunt Embed" style={{
+                  width: '250px',
+                  height: '54px'
+                }} width={250} height={54} />
+              </a>
+            </p>
+
           </Hero>
 
         </Section>
