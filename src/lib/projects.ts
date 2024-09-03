1b7128e76bf040542b0d637e0fa9761784abeb82
Colby Fayock <fayock@gmail.com>
2024-09-02 23:49:35 -0400

    cleanup

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,7 +1,5 @@
 import { promises as fs } from 'fs';
 import path from 'path'
-import { ReactNode } from 'react';
-import { PROJECT_TOPICS } from '@/data/projects';
 
 const PROJECTS_DIRECTORY = path.join(process.cwd(), 'src/projects');
 
@@ -15,24 +13,6 @@ interface Project {
   description?: string;
 }
 
-interface Topic {
-  title: string;
-  icon: ReactNode;
-  projects: Array<Project>;
-}
-
-/**
- * getProjects
- */
-
-export async function getProjects(): Promise<Array<Project>> {
-  const filenames = await fs.readdir(PROJECTS_DIRECTORY);
-  return Promise.all(filenames.map(async filename => {
-    const slug = filename.replace('.mdx', '');
-    return getProjectBySlug(slug);
-  }));
-}
-
 /**
  * getProjectBySlug
  */
@@ -44,31 +24,4 @@ export async function getProjectBySlug(slug: string): Promise<Project> {
     path: `/projects/${slug}`,
     mdx,
   }
-}
-
-/**
- * groupProjectsByTopic
- */
-
-export function groupProjectsByTopic(projects: Array<Project>) {
-  const topics: { [key: string]: Topic } = {};
-
-  projects.forEach(project => {
-    const projectTopic = project.topic || 'Unkonwn';
-
-    if ( !topics[projectTopic] ) {
-      const topic = PROJECT_TOPICS.find(({ title }) => title === projectTopic);
-
-      topics[projectTopic] = {
-        title: projectTopic,
-        icon: null,
-        projects: [],
-        ...topic,
-      };
-    }
-
-    topics[projectTopic].projects.push(project);
-  });
-
-  return PROJECT_TOPICS.map(({ title: key }) => topics[key]);
 }
